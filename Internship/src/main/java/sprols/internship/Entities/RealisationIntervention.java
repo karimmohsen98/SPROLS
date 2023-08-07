cbf5857be765f53cbf0da4f5042baf13bd68bb62
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-08-07 08:18:57 +0100

    entite realisation intervention

---
+++ Internship/src/main/java/sprols/internship/Entities/RealisationIntervention.java
@@ -0,0 +1,55 @@
+package sprols.internship.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class RealisationIntervention implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRealisationIntervnetion;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate dateRealisationIntervention;
+
+    @Null
+    @Column()
+    private String typeInternvetion;
+
+    @NotNull
+    @Column(nullable = false)
+    private int numMatriculeIntervenant;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate dateConstatDiagnostic;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate dateIntervention;
+
+    @Null
+    @Column
+    private Frequence frequence;
+
+    @Null
+    @Column()
+    private Origine origine;
+}
