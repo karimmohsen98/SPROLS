d91fd2e3834395f4aeca1c188ca1cfc14c5f4515
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-08-07 11:46:07 +0100

    Crud Role

--- Internship/src/main/java/sprols/internship/RestControllers/RoleController.java
+++ Internship/src/main/java/sprols/internship/RestControllers/RoleController.java
@@ -1,11 +1,27 @@
 package sprols.internship.RestControllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sprols.internship.Entities.Role;
+import sprols.internship.Services.IRoleServiceIMP;
 
 @RestController
 @RequestMapping("/api/role")
 @AllArgsConstructor
 public class RoleController {
+    private final IRoleServiceIMP iRoleServiceIMP;
+
+    @PostMapping("/ajouterrole")
+    public ResponseEntity<Object> ajouterRole(@RequestBody Role role){
+        return iRoleServiceIMP.ajoutRole(role);
+    }
+    @PostMapping("/modifierrole")
+    public ResponseEntity<Object> ModifierRole(@RequestBody Role role){
+        return iRoleServiceIMP.modifierRole(role);
+    }
+    @DeleteMapping("/supprimerrole/{idRole}")
+    public void removeAutomate(@PathVariable Integer idRole){
+        iRoleServiceIMP.supprimerRole(idRole);
+    }
 }
--- Internship/src/main/java/sprols/internship/Services/RoleService.java
+++ Internship/src/main/java/sprols/internship/Services/RoleService.java
@@ -1,4 +1,13 @@
 package sprols.internship.Services;
 
+import org.springframework.http.ResponseEntity;
+import sprols.internship.Entities.Role;
+
+import java.util.List;
+
 public interface RoleService {
+
+    ResponseEntity<Object> ajoutRole(Role role);
+    ResponseEntity<Object> modifierRole(Role role);
+    void supprimerRole(Integer idRole);
 }
